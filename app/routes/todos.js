47fb1a95111954aeb51655fcf0db88f90090db3b
Wajid Kagzi <wjkagzi@gmail.com>
2015-08-03 17:20:09 -0400

    Added the ability to add a new todo-item.

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -3,5 +3,21 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.find('todo');
+  },
+
+  actions: {
+    createToDo: function(newTitle) {
+      // Create new Todo model
+      var todo = this.store.createRecord('todo', {
+        title: newTitle,
+        isCompleted: false
+      });
+
+      // Clear the "New Todo" text field
+      this.controllerFor('todos').set('newTitle', '');
+
+      // Save the model
+      todo.save();
+    }
   }
 });
